75b7c3eb68ba98a99ca40dde45768a6296be75a0
Abhinav Sharma <abhi18av@gmail.com>
2017-11-07 11:33:37 +0530

    removed the name of the uberjar

--- project.clj
+++ project.clj
@@ -45,7 +45,6 @@
 ;; Concrete implementation of tools.analyzer
                  [org.clojure/tools.analyzer.jvm "0.7.0"]]
 
-  :uberjar-name "clojurejl.jar"
   :main ^:skip-aot clojurejl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
